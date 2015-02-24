276565a7c6528b3170adcb6a1f2424e3d6ee2d30
Matt Derrick <git@mattderrick.co.uk>
2015-02-24 20:48:41 +0000

    Component restructure and splitting routes to file ready for isomorphic

--- app/app.js
+++ app/app.js
@@ -1,28 +1,8 @@
 var React = require('react'),
 	Router = require('react-router'),
-	Layout = require('./components/layout/view'),
-	LoadingMixin = require('./mixins/loading'),
-	Index = require('react-proxy!./components/index/view'),
-	About = require('react-proxy!./components/about/view');
+	routes = require('./routes'),
+	css = require('app.css');
 
-require('app.css');
-
-/**
- * Proxied React views to mixin the
- * loading state whilst waiting for code splits.
- */
-var AboutProxy = React.createClass({
-    mixins: [About.Mixin, LoadingMixin]
-});
-var IndexProxy = React.createClass({
-    mixins: [Index.Mixin, LoadingMixin]
-});
-
-Router.run((
-	<Router.Route name="layout" handler={Layout} path='/'>
-		<Router.Route name="about" handler={AboutProxy} path='about' />
-        <Router.DefaultRoute name="index" handler={IndexProxy} />
-	</Router.Route>
-), Router.HistoryLocation, function (Handler) {
+Router.run(routes, Router.HistoryLocation, function (Handler) {
     React.render(<Handler/>, document.getElementById('main'));
 });
---
+++ app/routes.js
@@ -0,0 +1,26 @@
+var React = require('react'),
+	Router = require('react-router'),
+	Layout = require('./components/layout/view'),
+	LoadingMixin = require('./mixins/loading'),
+	Index = require('react-proxy!./components/layout/index/view'),
+	About = require('react-proxy!./components/layout/about/view');
+
+/**
+ * Proxied React views to mixin the
+ * loading state whilst waiting for code splits.
+ */
+var AboutProxy = React.createClass({
+    mixins: [About.Mixin, LoadingMixin]
+});
+var IndexProxy = React.createClass({
+    mixins: [Index.Mixin, LoadingMixin]
+});
+
+var routes = (
+	<Router.Route name="layout" handler={Layout} path='/'>
+		<Router.Route name="about" handler={AboutProxy} path='about' />
+        <Router.DefaultRoute name="index" handler={IndexProxy} />
+	</Router.Route>
+);
+
+module.exports = routes;
